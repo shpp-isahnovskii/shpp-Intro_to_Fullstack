0247a6454d4575f8eb969cef8e0398e058b16b99
Ivan <yaivan13@gmail.com>
2019-08-31 19:45:42 +0300

    forget what it was only half of the task. add sortBy. Finaly need to add filter by name

--- task2/static table/script/script.js
+++ task2/static table/script/script.js
@@ -2,7 +2,7 @@
 
 window.onload = () => { tableShuffleBy('category') };
 
-//table data
+//static table data
 const GOODS = [
   {
     category: 'furniture',
@@ -42,12 +42,14 @@ const GOODS = [
   }
 ];
 
+let FILTRED_GOODS = [];//changing table data
+
 let suffleDirection = false; //normal or reverse sort
 let currentFieldName = ''; //name of the clicked field in the head of the table
 
-//main function
 /**
- * get name of the field > look if need reverse > make sort by current field > rebuild table > bind reverse sort trigger to current field name
+ * ----------- Main function -----------
+ * get name of the field > look if need reverse > make sort by current field > rebuild table > bind reverse sort trigger to current field name ?????????
  * @param {name of the clicked field} fieldName 
  */
 function tableShuffleBy(fieldName) {
@@ -56,20 +58,24 @@ function tableShuffleBy(fieldName) {
     suffleDirection = !suffleDirection;
   }
 
-  tableSortBy(fieldName, suffleDirection);
-  tableBuild();
+  getSelectCategory();
+  tableSortBy(FILTRED_GOODS, fieldName, suffleDirection);
+  tableBuild(FILTRED_GOODS);
   addArrow(fieldName, suffleDirection);
+  getCost(FILTRED_GOODS);
 
   currentFieldName = fieldName; // set last clicked id name
 }
 
 
+
 /**
- * Sort function. 
+ * ----------- Sort -----------
+ * @param {get some table data to sort} table 
  * @param {table wil be sorted by this field} fieldName 
  * @param {reverse option} reverse 
  */
-function tableSortBy(fieldName, reverse) {
+function tableSortBy(table, fieldName, reverse) {
 
   /* result1,2 used for reverse sort*/
   let result1 = -1;
@@ -79,27 +85,56 @@ function tableSortBy(fieldName, reverse) {
       [result1, result2] = [result2, result1];
     }
 
-    GOODS.sort((a,b) => (a[fieldName] < b[fieldName] ? result1 : a[fieldName] > b[fieldName] ? result2 : 0) );  
+    table.sort((a,b) => (a[fieldName] < b[fieldName] ? result1 : a[fieldName] > b[fieldName] ? result2 : 0) );  
 }
 
-/**
- * Table build function.
+/** 
+ * ----------- HTML table builder -----------
  * Get table body from html > get table array from script > foreach rebuild > change body of the html table for new one
+ * @param {get some table data} table 
  */
-function tableBuild() {
+function tableBuild(table) {
   const oldTable = document.getElementById('tableBody');
   let newTable = '';
   
-  for(let atribute of GOODS) {
+  for(let atribute of table) {
     newTable += `<tr><td>${atribute.category}</td><td>${atribute.name}</td><td>${atribute.amount}</td><td>${atribute.price}</td></tr>`;
   }
   oldTable.innerHTML = newTable;
 }
 
+/** 
+ * ----------- Filter -----------
+ * Get inside of each '0' element of the table and search for category selected value
+ */
+function getSelectCategory() {
+  let selectedValue = document.getElementById('selectionList').value;
+
+  if(selectedValue == "") { //if value is empty - filter is not needed
+    FILTRED_GOODS = GOODS;
+  } else {
+    FILTRED_GOODS = GOODS.filter( (item)=> { return Object.entries(item)[0][1] == selectedValue;}); //0 - category, 1 - category name
+  }
+}
+
 /**
+ * ----------- Total $ -----------
+ * @param {current table} table 
+ */
+function getCost(table) { 
+  let total = 0;
+  for(let elem of table) {
+    total += elem.price * elem.amount;
+  }
+  document.getElementById('total').innerHTML = `${total}$`;
+}
+
+
+/** 
+ * ----------- arrows ▼ ▲-----------
  * Function add css class that print ▼ ▲ symbols; 
- * @param {*} id 
- * @param {*} arrowup 
+ * @param {current HTML id} id 
+ * @param {boolean for arrow} arrowup 
  */
 function addArrow(id, arrowup) {
   let thead = document.getElementById('tableHead').querySelectorAll('th'); //get all elements 'th' in table head
@@ -108,7 +143,6 @@ function addArrow(id, arrowup) {
     element.className = "noarrow";
   }
 
-  //select arrow direction
   if(arrowup) {
     document.getElementById(id).className = "arrowup";
   } else {
